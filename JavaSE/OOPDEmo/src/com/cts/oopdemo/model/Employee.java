5e19f05c57166ca7cbcfda7bac00bebae9cf2055
Aripaka Vamsy Kiran <a.vamc.it@gmail.com>
2021-02-04 12:36:22 +0530

    day7

--- JavaSE/OOPDEmo/src/com/cts/oopdemo/model/Employee.java
+++ JavaSE/OOPDEmo/src/com/cts/oopdemo/model/Employee.java
@@ -5,6 +5,25 @@ public class Employee {
 	private int empId;
 	private String fullName;
 	private double basic;
+			
+	public Employee() {
+		//default constructor as it has no args
+	}
+	
+	/* this is a parametrized constructor as it has args.
+	 * */
+	public Employee(int empId, String fullName, double basic) { 
+		this.empId = empId;
+		this.fullName = fullName;
+		this.basic = basic;
+	}
+	
+	//copy constructor
+	public Employee(Employee other) {
+		this.empId=other.empId;
+		this.fullName=other.fullName;
+		this.basic=other.basic;
+	}
 	
 	public int getEmpId() {
 		return empId;
--- JavaSE/OOPDEmo/src/com/cts/oopdemo/ui/EmployeeApplication.java
+++ JavaSE/OOPDEmo/src/com/cts/oopdemo/ui/EmployeeApplication.java
@@ -13,11 +13,29 @@ public class EmployeeApplication {
 		e1.setFullName("Vamsy");
 		e1.setBasic(45200.0);
 		
+		System.out.println(e1.getEmpId() + "\t" + e1.getFullName()+"\t"+e1.getBasic());
+		
+		
 		Employee e2 = new Employee();
 		e2.setEmpId(102);
 		e2.setFullName("Kiran");
 		e2.setBasic(45200.0);
 		
+		System.out.println(e2.getEmpId() + "\t" + e2.getFullName()+"\t"+e2.getBasic());
+		
+		Employee e3 = new Employee(103,"Srinu",528000.0); //object initialization.
+		System.out.println(e3.getEmpId() + "\t" + e3.getFullName()+"\t"+e3.getBasic());
+		
+		
+		Employee e4 = new Employee(e2);
+		
+		//e4=e2; is not goind to create a copy of e2
+		
+		/*
+		 * e4.setEmpId(e2.getEmpId()); e4.setFullName(e2.getFullName());
+		 * e4.setBasic(e2.getBasic());
+		 */		
+		System.out.println(e4.getEmpId() + "\t" + e4.getFullName()+"\t"+e4.getBasic());
 	}
 
 }
